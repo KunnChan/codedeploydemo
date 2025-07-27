0ff548b0bfabac0771b0b21e990d3deb196a5611
Min Kunn Chan <kunnchan3@gmail.com>
2025-07-27 16:24:55 +0800

    don't know already

---
+++ scripts/start_bkk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+APP_NAME=app.jar
+APP_DIR=/home/ec2-user/app
+LOG_FILE=$APP_DIR/app.log
+
+# Kill existing app
+PID=$(pgrep -f "$APP_NAME")
+if [ -n "$PID" ]; then
+  echo "Stopping existing application with PID $PID"
+  kill -9 $PID
+fi
+
+# Wait for the process to stop
+sleep 2
+
+# Start new application
+echo "Starting new application..."
+nohup java -jar $APP_DIR/$APP_NAME > $LOG_FILE 2>&1 &
